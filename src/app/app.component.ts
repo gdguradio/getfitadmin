c4dcb0b460caf3e1eea7377e03246e105f796243
Martin Lloyd Manlapig <gdguradio@gmail.com>
2018-08-14 09:54:44 +0800

    Final Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'getfitadmin';
+  title = 'Getfit247 Admin';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'; 
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { UserService } from './user.service';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [AuthService,UserService,AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
